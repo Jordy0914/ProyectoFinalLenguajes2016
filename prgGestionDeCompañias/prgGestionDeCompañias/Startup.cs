2e0ea9aad1a30b72cef8763664524b03080a914b
Jordy Garcia Mendoza <jordy0914@hotmail.com>
2016-06-06 14:10:26 -0600

    Commit initial

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prgGestionDeCompañias.Startup))]
+namespace prgGestionDeCompañias
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
