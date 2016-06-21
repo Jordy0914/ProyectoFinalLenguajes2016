1d366af45762610ae28f36abaf16092e83a0becc
Bryan <Bryan@BRYANMQ>
2016-06-20 22:59:53 -0600

    Models

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Models/ProyectoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prgGestionDeCompañias.Models
+{
+    public class ProyectoModel
+    {
+        public int idEmpresa { get; set; }
+        public int idProyecto { get; set; }
+        public int horasInvertidas { get; set; }
+    }
+}
