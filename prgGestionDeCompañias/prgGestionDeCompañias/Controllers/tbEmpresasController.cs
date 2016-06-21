e45b3072923b4545b225b985d929334dab900748
Bryan <Bryan@BRYANMQ>
2016-06-20 23:18:07 -0600

    .

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Controllers/tbEmpresasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using prgGestionDeCompañias;
+
+namespace prgGestionDeCompañias.Controllers
+{
+    public class tbEmpresasController : Controller
+    {
+        private BDPortafolioUcrContext db = new BDPortafolioUcrContext();
+
+        // GET: tbEmpresas
+        public ActionResult Index()
+        {
+            return View(db.tbEmpresas.ToList());
+        }
+
+        // GET: tbEmpresas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbEmpresas tbEmpresas = db.tbEmpresas.Find(id);
+            if (tbEmpresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbEmpresas);
+        }
+
+        // GET: tbEmpresas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tbEmpresas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEmpresa,nombre,descripcionGene,foto")] tbEmpresas tbEmpresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbEmpresas.Add(tbEmpresas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbEmpresas);
+        }
+
+        // GET: tbEmpresas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbEmpresas tbEmpresas = db.tbEmpresas.Find(id);
+            if (tbEmpresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbEmpresas);
+        }
+
+        // POST: tbEmpresas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEmpresa,nombre,descripcionGene,foto")] tbEmpresas tbEmpresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbEmpresas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbEmpresas);
+        }
+
+        // GET: tbEmpresas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbEmpresas tbEmpresas = db.tbEmpresas.Find(id);
+            if (tbEmpresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbEmpresas);
+        }
+
+        // POST: tbEmpresas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbEmpresas tbEmpresas = db.tbEmpresas.Find(id);
+            db.tbEmpresas.Remove(tbEmpresas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- prgGestionDeCompañias/prgGestionDeCompañias/Models/EmpresaModel.cs
+++ prgGestionDeCompañias/prgGestionDeCompañias/Models/EmpresaModel.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //Permite realizar validaciones a nuestra clase a nivel del models.
+using System.Globalization; //Biblioteca de configuracion del uso de varios idiomas.
 
 namespace prgGestionDeCompañias.Models
 {
     public class EmpresaModel
     {
-
+        [Required] //Valida la entrada de datos al campo
+        [StringLength(5)]//Tamaño de la cadena
+        [Display(Name = "Identificación de la Empresa")]
         public int idEmpresa { get; set; }
+
+        [Required]
+        [StringLength(100)]//Tamaño de la cadena
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracteres inválidos.")]
+        [Display(Name = "Nombre de la Empresa:")]
         public string nombre { get; set; }
         public string descripcionGene { get; set; }
         public byte[] foto { get; set; }
