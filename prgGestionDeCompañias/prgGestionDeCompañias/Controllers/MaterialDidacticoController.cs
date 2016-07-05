8d37563bcd55cead4fb4c05cb80bc9b5a56c5b0a
josseprado <josselineprado2@gmail.com>
2016-07-05 11:28:38 -0600

    Material didactico

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Controllers/MaterialDidacticoController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using models = prgGestionDeCompañias.Models;
+//using ent = prgGestionDeCompañias_Entidad;
+
+
+namespace prgGestionDeCompañias.Controllers
+{
+    public class MaterialDidacticoController : Controller
+    {
+        // GET: MaterialDidactico
+        public ActionResult Index()
+
+        {
+            BDPortafolioUcrContext dbContext = new BDPortafolioUcrContext();
+            //realizo la consulta
+            var consulta = from datos in dbContext.tbArchivos
+                           orderby datos.idArchivo ascending
+                           select datos;
+            //se crea la una coleccion de elementos
+
+            IEnumerable<tbArchivos> archivos = consulta.ToList();
+
+            return View(archivos);         
+        }
+
+        // GET: MaterialDidactico/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MaterialDidactico/Create
+        public ActionResult AgregarMaterial()
+        {
+            BDPortafolioUcrContext db = new BDPortafolioUcrContext();
+            Models.AgregarMaterialDidactico datos = new Models.AgregarMaterialDidactico();
+            //obtengo el ultimo numero del id de la tabla y le sumo 1
+            //para hacer un consecutivo
+
+            var maximo = ((from codigo in db.tbMaterialesDida select (int?)codigo.idMaterialDida).Max()) + 1;
+
+            //lo convietro en string y se lo asigno al al id de la tabla
+            //del modelo creado por nosotros
+            if (maximo == 0)
+            {
+                maximo = 1;
+                datos.idMaterialDidactico = Convert.ToInt32(1);
+                return View(datos);
+            }
+            else
+            {
+                datos.idMaterialDidactico = Convert.ToInt32(maximo);
+                return View(datos);
+            }
+
+        }
+
+        // POST: MaterialDidactico/Create
+
+
+        [HttpPost]
+        public ActionResult AgregarMaterial(models.AgregarMaterialDidactico material)
+        {
+           
+                // TODO: Add insert logic here
+               
+
+                if (ModelState.IsValid)
+                {
+                    using (var db = new BDPortafolioUcrContext())
+                    {
+
+                    var materiales = db.tbMaterialesDida.Create();
+                    var archivos = db.tbArchivos.Create();
+                    var autores = db.tbAutores.Create();
+
+                    byte[] data = new byte[material.archivo.ContentLength];
+                    material.archivo.InputStream.Read(data, 0, material.archivo.ContentLength);
+
+                
+                    archivos.idArchivo = Convert.ToInt32(4);//
+                    archivos.archivo = data;
+
+
+                    // material.contenido = material.archivo.ContentType;
+                    // archivos.archivo = material.archivo.ContentType;
+
+                    materiales.nombreArch = material.archivo.FileName;
+                    int cod = Convert.ToInt32(materiales.idMaterialDida);
+                   // materiales.idMaterialDida = Convert.ToInt32(material.idMaterialDidactico);
+                    materiales.titulo = material.titulo;
+                    materiales.fechaPubl = Convert.ToDateTime( material.fechaPublicacion);
+                    materiales.ciudad = material.cuidad;
+                    materiales.pais = material.pais;
+
+
+                   
+                    autores.nombre = material.autor;
+                    autores.idAutor = Convert.ToInt32(3);
+                    
+
+                    db.tbMaterialesDida.Add(materiales);
+                    db.tbArchivos.Add(archivos);
+                    db.tbAutores.Add(autores);
+
+                    db.SaveChanges();
+                   
+                    return RedirectToAction("AgregarMaterial", "MaterialDidactico");
+                        //return RedirectToAction("Index"); es que traia por defecto
+
+               }//fin del using
+         }//fin del if valid
+
+         else
+           {
+              ModelState.AddModelError("", "El registro fue incorrecto");
+           }
+
+            return View();
+        }//fin del metodo para agregar material didactico
+
+        // GET: MaterialDidactico/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MaterialDidactico/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MaterialDidactico/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MaterialDidactico/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        public FileContentResult DescargarArchivo(int id)
+        {
+
+            Byte[] data;
+            String nombre;
+
+            using (var db = new BDPortafolioUcrContext())
+            {
+
+                var archivos = db.tbArchivos.Create();
+                var materiales = db.tbMaterialesDida.Create();
+
+            data = (Byte[])archivos.archivo.ToArray();
+                nombre = materiales.nombreArch;
+          }
+
+            return File(data,"Text",nombre);
+        }
+
+    }
+}
---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Models/materialDidacticoModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;// permite realizar validaciones a nuestra clase a nivel del models
+using System.Globalization;//biblioteca de configuracion del uso de varios idiomas
+using System.Web;
+
+namespace prgGestionDeCompañias.Models
+{
+    public class AgregarMaterialDidactico
+    {
+
+        [Required]//valida la entrada de datos al campo
+      //  [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracter invalido")]
+       // [StringLength(5)]
+        [Display(Name = "Codigo")]
+        public int idMaterialDidactico { get; set; }
+
+
+        //[Required]
+        //[StringLength(100)]
+        //[Display(Name = "Nombre del Archivo")]
+        public string nombreArchivo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Titulo")]
+        public string titulo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Autor")]
+        public string autor { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        //[RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracter invalido")]
+        [Display(Name = "Fecha de Publicacion")]
+        public string fechaPublicacion { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Cuidad")]
+        public string cuidad { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Pais")]
+        public string pais { get; set; }
+
+       
+        public HttpPostedFileBase archivo { get; set; }
+        public byte[] manual { get; set; }
+        public string contenido { get; set; }
+
+        
+
+    }//fin de la clase Registrar Material Didactico
+
+
+
+    public class ModificarMaterialDidactico
+    {
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre del Archivo")]
+        public string nombreArchivo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Titulo")]
+        public string titulo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracter invalido")]
+        [Display(Name = "Fecha de Publicacion")]//es lo que sale en la pagina web como un label
+        public string fechaPublicacion { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Cuidad")]
+        public string cuidad { get; set; }
+
+
+        [Required]//indica que este valor es requerido y no debe de quedar vacio
+        [StringLength(100)]//indica el maximo de caracteres
+        [Display(Name = "Pais")]
+        public string pais { get; set; }
+
+
+    }//fin de la clase modificar material didactico
+
+
+    public class ConsultarMaterialDidactico
+    {
+
+
+        [Required]//valida la entrada de datos al campo
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracter invalido")]
+        [StringLength(5)]
+        [Display(Name = "Codigo")]//es lo que sale en la pagina web como un label
+        public int idMaterialDidactico { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre del Archivo")]//es lo que sale en la pagina web como un label
+        public string nombreArchivo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Titulo")]//es lo que sale en la pagina web como un label
+        public string titulo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracter invalido")]
+        [Display(Name = "Fecha de Publicacion")]//es lo que sale en la pagina web como un label
+        public string fechaPublicacion { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Cuidad")]//es lo que sale en la pagina web como un label
+        public string cuidad { get; set; }
+
+
+        [Required]//indica que este valor es requerido y no debe de quedar vacio
+        [StringLength(100)]//indica el maximo de caracteres
+        [Display(Name = "Pais")]//es lo que sale en la pagina web como un label
+        public string pais { get; set; }
+
+    }//fin de la clase consultar material didactico
+
+
+}
+
