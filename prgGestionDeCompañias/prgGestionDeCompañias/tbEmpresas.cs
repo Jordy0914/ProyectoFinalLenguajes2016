6b0cbf64a70dfcd964d650e2dab20aa0fca3f262
josseprado <josselineprado2@gmail.com>
2016-07-07 10:58:34 -0600

    Agrega material didactico

--- prgGestionDeCompañias/prgGestionDeCompañias/tbEmpresas.cs
+++ prgGestionDeCompañias/prgGestionDeCompañias/tbEmpresas.cs
@@ -6,7 +6,6 @@
 //     Los cambios manuales en este archivo se sobrescribirán si se regenera el código.
 // </auto-generated>
 //------------------------------------------------------------------------------
-using System.ComponentModel.DataAnnotations;
 
 namespace prgGestionDeCompañias
 {
@@ -28,27 +27,12 @@ namespace prgGestionDeCompañias
             this.tbConvenios = new HashSet<tbConvenios>();
             this.tbMaterialesDida = new HashSet<tbMaterialesDida>();
         }
-
-        [Required]
-        [StringLength(5)]
-        [Display(Name = "Identificación de la Empresa")]
+    
         public int idEmpresa { get; set; }
-
-        [Required]
-        [StringLength(100)]//Tamaño de la cadena
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracteres inválidos.")]
-        [Display(Name = "Nombre de la Empresa:")]
         public string nombre { get; set; }
-
-        [Required]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Caracteres inválidos.")]
-        [StringLength(100)]
-        [Display(Name = "Descripción General:")]
         public string descripcionGene { get; set; }
-
-        [Display(Name = "Foto/Logo:")]
         public byte[] foto { get; set; }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tbBolsasEmpl> tbBolsasEmpl { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
