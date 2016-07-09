2a1226a37a18c63c1f479b20bb779c3892ac1f97
Miguel <Miguel@DESKTOP-A1FRNG4>
2016-07-09 17:33:23 -0600

    guarda capacitacion

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Controllers/CapacitacionController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prgGestionDeCompañias.Controllers
+{
+    public class CapacitacionController : Controller
+    {
+        // GET: Capacitacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Capacitacion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Capacitacion/Create
+        public ActionResult Insertar()
+        {
+            return View();
+        }
+
+        // POST: Capacitacion/Create
+        [HttpPost]
+        public ActionResult Insertar(Models.CapacitacionModels capacitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new BDPortafolioUcrContext())
+                {
+                    var temp = db.tbCapacitaciones.Create();
+                    temp.id = capacitacion.Id;
+                    temp.nombre = capacitacion.Nombre;
+                    temp.descripcion = capacitacion.Descripcion;
+                    temp.ubicacion = capacitacion.Ubicacion;
+                    temp.idPersona = capacitacion.IdPersona;
+                    temp.fechaInic = capacitacion.FechaInic;
+                    temp.fechaFin = capacitacion.FechaFin;
+                    temp.inversion = capacitacion.Inversion;
+                    temp.tipoCertificado = capacitacion.TipoCertificado;
+                    db.tbCapacitaciones.Add(temp);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index", "Home");
+                } //fin del using
+            }// fin del if
+            else
+            {
+                ModelState.AddModelError("", "El registro fue incorrecto");
+            }
+        return View();    
+        }
+
+        // GET: Capacitacion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Capacitacion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Capacitacion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Capacitacion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Models/CapacitacionModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prgGestionDeCompañias.Models
+{
+    public class CapacitacionModels
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Ubicacion { get; set; }
+        public string IdPersona { get; set; }
+        public System.DateTime FechaInic { get; set; }
+        public System.DateTime FechaFin { get; set; }
+        public int Inversion { get; set; }
+        public string TipoCertificado { get; set; }
+
+    }
+}
+
+
