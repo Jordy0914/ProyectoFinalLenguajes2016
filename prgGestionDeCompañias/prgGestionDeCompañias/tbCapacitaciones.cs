f27cb7cc965ee50fa6096c68670300ac8945e207
Bryan <Bryan@BRYANMQ>
2016-07-09 15:12:10 -0600

    commit Final

--- prgGestionDeCompañias/prgGestionDeCompañias/tbCapacitaciones.cs
+++ prgGestionDeCompañias/prgGestionDeCompañias/tbCapacitaciones.cs
@@ -23,7 +23,7 @@ namespace prgGestionDeCompañias
             this.tbHorarios = new HashSet<tbHorarios>();
         }
     
-        public int idCapacitacion { get; set; }
+        public int id { get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
         public string ubicacion { get; set; }
