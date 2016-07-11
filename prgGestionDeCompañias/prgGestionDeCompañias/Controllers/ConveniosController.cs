02092180754b34d8d3f5f2b575eb52de9f8296b0
Bryan <Bryan@BRYANMQ>
2016-07-11 11:42:04 -0600

    .

---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Controllers/ConveniosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using prgGestionDeCompañias;
+
+namespace prgGestionDeCompañias.Controllers
+{
+    public class ConveniosController : Controller
+    {
+        private BDPortafolioUcrContext db = new BDPortafolioUcrContext();
+
+        // GET: Convenios
+        public ActionResult Index()
+        {
+            return View(db.tbConvenios.ToList());
+        }
+
+        // GET: Convenios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbConvenios tbConvenios = db.tbConvenios.Find(id);
+            if (tbConvenios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbConvenios);
+        }
+
+        // GET: Convenios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Convenios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idConvenio,descripcion,fechaCrea,estado")] tbConvenios tbConvenios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbConvenios.Add(tbConvenios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbConvenios);
+        }
+
+        // GET: Convenios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbConvenios tbConvenios = db.tbConvenios.Find(id);
+            if (tbConvenios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbConvenios);
+        }
+
+        // POST: Convenios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idConvenio,descripcion,fechaCrea,estado")] tbConvenios tbConvenios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbConvenios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbConvenios);
+        }
+
+        // GET: Convenios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbConvenios tbConvenios = db.tbConvenios.Find(id);
+            if (tbConvenios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbConvenios);
+        }
+
+        // POST: Convenios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbConvenios tbConvenios = db.tbConvenios.Find(id);
+            db.tbConvenios.Remove(tbConvenios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ prgGestionDeCompañias/prgGestionDeCompañias/Models/ConveniosModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;// permite realizar validaciones a nuestra clase a nivel del models
+using System.Globalization;//biblioteca de configuracion del uso de varios idiomas
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace prgGestionDeCompañias.Models
+{
+    public class AgregarConveniosModels
+    {
+		[Required]
+        [Display(Name = "idConvenio:")]
+		public int idConvenio { get; set; }
+		
+		[Required]
+		[StringLength(500)]
+		[RegularExpression(@"^\$?\d+(\.(\d{2}))?$")]
+        [Display(Name = "Descripcion:")]
+		public string descripcion { get; set; }
+		
+
+		[Required (ErrorMessage = "seleccione la fecha")]
+        [Display(Name = "Fecha creacion")]
+		public System.DateTime fechaCrea { get; set; }
+		
+		[Required (ErrorMessage = "seleccione el estado")]
+        [StringLength(500)]
+        [Display(Name = "Estado")]
+		public string estado { get; set; }	
+		
+		
+		
+    }
+}
